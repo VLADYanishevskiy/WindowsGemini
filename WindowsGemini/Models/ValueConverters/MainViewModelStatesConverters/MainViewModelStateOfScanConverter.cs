42786c4f9c2610ae892b454e1b69d99406a6bdb4
Vlad <Vlad@DESKTOP-68MAU13>
2020-09-01 15:54:36 +0300

    Added x:Uid for button scan fro duplicates. Added translte for this button. Replaced using for multiviewController for VisualStates int grid.

---
+++ WindowsGemini/Models/ValueConverters/MainViewModelStatesConverters/MainViewModelStateOfScanConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace WindowsGemini.Models.ValueConverters.MainViewModelStatesConverters
+{
+    class MainViewModelStateOfScanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            int? iValue = value as int?;
+            int iPam = int.Parse(parameter.ToString());
+
+            if(iValue.HasValue)
+            {
+                return iValue.Value == iPam;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
